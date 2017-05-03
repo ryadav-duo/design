586041d026d5ce1c7e345dbd0a7f592f117fb2e2
rcardneau <rcardneau@duosecurity.com>
2017-05-03 09:58:44 -0400

    cleaning up the gulp file and adding the reference images

--- gulp.js
+++ gulp.js
@@ -1,25 +1,8 @@
-const browserSync = require('browser-sync').create()
 const cssnano = require('cssnano')
 const gulp = require('gulp')
-const htmlmin = require('gulp-htmlmin')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
-const jsmin = require('gulp-jsmin')
 const rename = require('gulp-rename')
-
-const json = require('./scripts/render_json')
-const page = require('./scripts/render_page')
-
-
-gulp.task('pages', () => (
-    gulp.src('assets/templates/*.html')
-        .pipe(page('assets/templates'))
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            conservativeCollapse: true,
-        }))
-        .pipe(gulp.dest('dist'))
-))
 
 
 gulp.task('styles', () => (
@@ -28,26 +11,26 @@ gulp.task('styles', () => (
         .pipe(postcss([
             cssnano()
         ]))
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/styles'))
-        .pipe(browserSync.stream())
 ))
 
 gulp.task('referenceFiles', () => {
-  gulp.src('assets/styles/**/*.css')
-      .pipe(gulp.dest('dist/styles'))
-      .pipe(browserSync.stream())
-  gulp.src('assets/js/reference/**/**/*.js')
-      .pipe(jsmin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('dist/js'))
+    gulp.src('assets/styles/**/*.css')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/styles'))
+  // gulp.src('assets/js/reference/**/**/*.js')
+  //     .pipe(jsmin())
+  //     .pipe(rename({suffix: '.min'}))
+  //     .pipe(gulp.dest('dist/js'))
 })
 
-gulp.task('scripts', () => (
-    gulp.src('assets/js/general.js')
-        .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/js'))
-))
+// gulp.task('scripts', () => (
+//     gulp.src('assets/js/general.js')
+//         .pipe(jsmin())
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(gulp.dest('dist/js'))
+// ))
 
 
 gulp.task('images', () => (
@@ -60,30 +43,7 @@ gulp.task('fonts', () => (
         .pipe(gulp.dest('dist/fonts'))
 ))
 
-gulp.task('json', () => (
-  gulp.src('assets/json/*.json')
-    // .pipe(json('assets/json/'))
-    .pipe(gulp.dest('dist/json'))
-))
 
-gulp.task('build', ['pages', 'json', 'styles', 'scripts', 'images', 'fonts', 'referenceFiles'])
+gulp.task('build', ['styles', 'images', 'fonts', 'referenceFiles'])
 
-gulp.task('refresh', ['pages', 'styles', 'scripts'])
-
-
-gulp.task('watch', ['refresh'], () => {
-    gulp.watch([
-        'assets/pages/**/*.md',
-        'assets/templates/**/*.html',
-    ], ['pages']).on('change', browserSync.reload)
-
-    gulp.watch('assets/styles/**/**/*.scss', ['styles'])
-    gulp.watch('assets/js/**/*.js', ['scripts'])
-
-    browserSync.init({
-        server: {
-            baseDir: 'dist',
-        },
-        open: false,
-    })
-})
+gulp.task('refresh', ['styles', 'images'])
