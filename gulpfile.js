17b8ee80b0e5087e3b02eea965144912f8c60543
rcardneau <rcardneau@duosecurity.com>
2017-07-18 11:13:02 -0400

    brand spanking new changes for DPL as a dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,47 @@
 const browserSync = require('browser-sync').create()
 const cssnano = require('cssnano')
 const gulp = require('gulp')
-const htmlmin = require('gulp-htmlmin')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
-const jsmin = require('gulp-jsmin')
 const rename = require('gulp-rename')
 
-const json = require('./scripts/render_json')
-const page = require('./scripts/render_page')
-
-
-gulp.task('pages', () => (
-    gulp.src('assets/templates/*.html')
-        .pipe(page('assets/templates'))
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            conservativeCollapse: true,
-        }))
-        .pipe(gulp.dest('dist'))
-))
-
-
 gulp.task('styles', () => (
-    gulp.src('assets/styles/**/*.scss')
+    gulp.src('patterns/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(postcss([
             cssnano()
         ]))
-        .pipe(gulp.dest('dist/styles'))
+        .pipe(gulp.dest('dist/patterns'))
         .pipe(browserSync.stream())
-))
-
-gulp.task('referenceFiles', () => {
-  gulp.src('assets/styles/**/*.css')
-      .pipe(gulp.dest('dist/styles'))
-      .pipe(browserSync.stream())
-  gulp.src('assets/js/reference/**/**/*.js')
-      .pipe(jsmin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('dist/js'))
-})
-
-gulp.task('scripts', () => (
-    gulp.src('assets/js/general.js')
-        .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/js'))
 ))
 
 
 gulp.task('images', () => (
-    gulp.src('assets/images/*')
+    gulp.src('images/*')
         .pipe(gulp.dest('dist/images'))
 ))
 
 gulp.task('fonts', () => (
-    gulp.src('assets/fonts/**/*')
+    gulp.src('fonts/**/*')
         .pipe(gulp.dest('dist/fonts'))
 ))
 
-gulp.task('json', () => (
-  gulp.src('assets/json/*.json')
-    // .pipe(json('assets/json/'))
-    .pipe(gulp.dest('dist/json'))
+gulp.task('font-styles', () => (
+    gulp.src('fonts/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(postcss([
+            cssnano()
+        ]))
+        .pipe(gulp.dest('dist/fonts'))
 ))
 
-gulp.task('build', ['pages', 'json', 'styles', 'scripts', 'images', 'fonts', 'referenceFiles'])
+gulp.task('build', ['styles','images', 'fonts', 'font-styles'])
 
-gulp.task('refresh', ['pages', 'styles', 'scripts'])
+gulp.task('refresh', ['styles'])
 
 
 gulp.task('watch', ['refresh'], () => {
-    gulp.watch([
-        'assets/pages/**/*.md',
-        'assets/templates/**/*.html',
-    ], ['pages']).on('change', browserSync.reload)
-
-    gulp.watch('assets/styles/**/**/*.scss', ['styles'])
-    gulp.watch('assets/js/**/*.js', ['scripts'])
+    gulp.watch('patterns/**/**/*.scss', ['styles'])
 
     browserSync.init({
         server: {
